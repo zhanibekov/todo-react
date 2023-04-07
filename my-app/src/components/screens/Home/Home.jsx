dab431c6f104f469dbb24117e976f000686b479c
yelaman <67456840+zhanibekov@users.noreply.github.com>
2023-04-07 21:29:24 +0600

    Hello

--- my-app/src/components/screens/Home/Home.jsx
+++ my-app/src/components/screens/Home/Home.jsx
@@ -1,25 +1,37 @@
 import React from 'react'
+import TodoItem from './TodoItem'
 
 const todos = [
 {
+  _id: 'wefw23232',
   title: "Start",
   isCompleted: false,
 },
 {
-  title: 'Finish the essay collabaration',
+  _id: 'wefw23233',
+  title: 'in progress',
   isCompleted: false,
 },
 {
+  _id: 'wefw23234',
   title: 'Home Work',
   isCompleted: false,
 },
+{
+  _id: 'wefw23235',
+  title: 'Finish the essay Abay Kunanbayev',
+  isCompleted: false,
+}
 ]
 
 const Home = () => {
-  return <div className='bg-gray-900 h-screen text-white'>Home
-  
-  {todos.map(todo => (<div>{todo.title}</div>))}
+  return ( 
+  <div className='bg-gray-900 h-screen text-white'>
+  {todos.map(todo =>(
+    <TodoItem key={todo._id} todo={todo} />
+  ))}
   </div>
+  )
 }
 
 
---
+++ my-app/src/components/screens/Home/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodoItem = ({ todo }) => {
+  return (
+    <div>{todo.title}</div>
+  )
+}
+
+export default TodoItem
